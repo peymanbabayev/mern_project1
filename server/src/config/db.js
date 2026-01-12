0901c2d05c19e075c2330f643c5a9c3907b5cd55
Peyman Babayev <peyman.babayev.01@mail.ru>
2026-01-12 14:21:00 +0400

    Update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import config from "./config.js";
 
 const connectDB = async () => {
-  const MONGO_URL = process.env.MONGO_URL;
+  const MONGO_URL = config.database.mongoUrl;
   if (!MONGO_URL) throw new Error("MONGO_URL is not defined in environment variables");
-  
+
   try {
     await mongoose.connect(MONGO_URL);
     console.log("MongoDB connected successfully");
